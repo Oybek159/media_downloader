74957997dde80cce8f9dd2e3d0f9b4e6ce914944
Oybek159 <oybekxalimov159@gmail.com>
2025-12-04 11:15:42 +0500

    first

--- handlers/users/instagram_download.py
+++ handlers/users/instagram_download.py
@@ -1,19 +1,35 @@
 from aiogram import Router, F
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from utils.instagram_service import downloader
+from keyboards.inline_keyboards import get_music
 
 router = Router()
+
+data = ()
 
 @router.message(F.text.startswith(("https://www.instagram.com/", "https://instagram.com")))
 async def insta_downloader(msg: Message):
     await msg.answer("⌛️")
+    global data
     data = downloader(link=msg.text)
+    # await msg.answer(str(data))
     
     for type in data:
         if type == "error":
             await msg.answer("Invalid link")
-        if type["type"] == "image":
-            await msg.answer_photo(photo=type["media"])
-        elif type["type"] == "video":
-            await msg.answer_video(video=type["media"])
-    print(data)
+        if type[0]["type"] == "image":
+            await msg.answer_photo(photo=type[0]["media"])
+        elif type[0]["type"] == "video":
+            await msg.answer_video(video=type[0]["media"], reply_markup=get_music())
+
+    
+    
+    # print(data)
+
+@router.callback_query(F.data=="music")
+async def get_video_func(call: CallbackQuery):
+    for type in data:
+        if type[0]["type"] == "audio":
+            await call.message.answer_audio(audio=type[0]["audio"])
+        else:
+            await call.message.answer("Music not found")
